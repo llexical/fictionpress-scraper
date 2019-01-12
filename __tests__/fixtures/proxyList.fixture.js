e7bbe86e20f399e41fdc704d73a9da4feabe4b86
Lizzie <lizzie@hubblehq.com>
2019-01-12 13:58:09 +0000

    Add fixtures in, I forgot to add them xD.

---
+++ __tests__/fixtures/proxyList.fixture.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    ipAddress: "37.59.8.29",
+    port: "19571",
+    protocol: "SOCKS5",
+    responseTime: 161
+  },
+  {
+    ipAddress: "41.33.190.100",
+    port: "9999",
+    protocol: "SOCKS4",
+    responseTime: 662
+  },
+  {
+    ipAddress: "50.62.35.10",
+    port: "29482",
+    protocol: "SOCKS5",
+    responseTime: 10000
+  }
+];
