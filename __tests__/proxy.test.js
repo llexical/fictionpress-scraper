4e66893a1fe8b6d589d170664705c78449416972
Lizzie <lizzie@hubblehq.com>
2019-01-12 13:15:23 +0000

    Add functionality to get a proxy conn string, also husky and prettier.
    To get the best proxy I need to order the list by the best
    response times and then return the fastest. This means that
    when we make calls to get the pages we will doing it with our
    best possible option. Obviously due to the location of the proxy
    sites hosting its response times will be biased I assume but
    its the best approximation for now.
    
    Also added husky and prettier for formatting and to run tests on commit
    and push.

--- __tests__/proxy.test.js
+++ __tests__/proxy.test.js
@@ -1,42 +1,69 @@
 const fs = require("fs");
 const request = require("request-promise");
-const { getProxyList } = require("../proxy");
+const proxy = require("../proxy");
 
-const proxyListHtml = require("./fixtures/proxyListHtml");
+const proxyListFixture = require("./fixtures/proxyList.fixture");
+const proxyListHtml = require("./fixtures/proxyListHtml.fixture");
 
 jest.mock("request-promise");
 request.mockImplementation(async () => proxyListHtml);
 
 describe("proxy :: getProxyList", () => {
   it("should get an array of proxies", async () => {
-    const proxyList = await getProxyList();
+    const proxyList = await proxy.getProxyList();
 
     expect(proxyList).toBeInstanceOf(Array);
     expect(proxyList.length).toBeGreaterThan(0);
   });
 
   describe("each proxy item", () => {
-    let proxy;
+    let proxyObj;
 
     beforeAll(async () => {
-      const proxyList = await getProxyList();
-      proxy = proxyList[0];
+      const proxyList = await proxy.getProxyList();
+      proxyObj = proxyList[0];
     });
 
     it("should have ip address", () => {
-      expect(proxy).toHaveProperty("ipAddress");
+      expect(proxyObj).toHaveProperty("ipAddress");
     });
 
     it("should have port", () => {
-      expect(proxy).toHaveProperty("port");
+      expect(proxyObj).toHaveProperty("port");
     });
 
     it("should have socks protocol", () => {
-      expect(proxy).toHaveProperty("protocol");
+      expect(proxyObj).toHaveProperty("protocol");
     });
 
-    it("should have response times", () => {
-      expect(proxy).toHaveProperty("responseTimes");
+    it("should have response time", () => {
+      expect(proxyObj).toHaveProperty("responseTime");
     });
+
+    it("should be the same as if a human extracted the data", () => {
+      expect(proxyObj).toEqual(proxyListFixture[0]);
+    });
+  });
+});
+
+describe("proxy :: getProxy", () => {
+  beforeEach(async () => {
+    proxy.getProxyList = jest.fn(async () => proxyListFixture);
+    proxy.proxy = null; // clear the proxy cache
+  });
+
+  it("should return a single proxy connection string", async () => {
+    const proxyString = await proxy.getProxy();
+
+    expect(proxy.getProxyList).toBeCalledTimes(1);
+    expect(proxyString).toEqual("SOCKS5://37.59.8.29:19571");
+  });
+
+  it("should return the same result from the cache", async () => {
+    await proxy.getProxy();
+    const proxyString = await proxy.getProxy();
+
+    expect(proxy.getProxyList).toBeCalledTimes(1);
+    expect(proxyString).toEqual("SOCKS5://37.59.8.29:19571");
   });
 });
--- proxy.js
+++ proxy.js
@@ -7,51 +7,63 @@ const { appendFileAsync, createFolder } = require("./helpers.js");
 
 const proxyListUrl = "http://www.gatherproxy.com/sockslist";
 
-/**
- * Gets a list of available free socks proxy servers from
- * http://www.gatherproxy.com/sockslist which has been up
- * since 2013 and seemed pretty stable, no design changes etc.
- *
- * TODO: Want to sort them by response time reverse :).
- *
- * @returns Array - Array of proxy items
- */
-async function getProxyList() {
-  const proxyList = [];
-  const response = await request(proxyListUrl);
+class Proxy {
+  /**
+   * Gets a list of available free socks proxy servers from
+   * http://www.gatherproxy.com/sockslist which has been up
+   * since 2013 and seemed pretty stable, no design changes etc.
+   *
+   * TODO: Want to sort them by response time reverse :).
+   *
+   * @returns Array - Array of proxy items
+   */
+  async getProxyList() {
+    const proxyList = [];
+    const response = await request(proxyListUrl);
 
-  // Get table rows
-  const tableRows = $("#tblproxy tr", response);
+    // Get table rows
+    const tableRows = $("#tblproxy tr", response);
 
-  // Data
-  tableRows.each((i, row) => {
-    // The first 2 rows are headings and ads
-    if (i <= 2) return;
+    // Data
+    tableRows.each((i, row) => {
+      // The first 2 rows are headings and ads
+      if (i < 2) return;
 
-    const rowData = [];
-    $("td", row).each((i, col) => {
-      rowData.push($(col).text() || $(col).html());
+      const rowData = [];
+      $("td", row).each((i, col) => {
+        rowData.push($(col).text() || $(col).html());
+      });
+
+      const [, ipAddress, port, , , protocol, responseTimes] = rowData;
+      /**
+       * IP address and port are both stored in a strange script tag
+       * I think to make it harder to scrape, I guess if they change
+       * it will break, but seems fine for now, pretty simple regex.
+       *
+       * Possible socks Num formats are currently: "socks4", "socks4/5", "socks5".
+       */
+      proxyList.push({
+        ipAddress: ipAddress.match(/([0-9]+\.?)+/g)[0],
+        port: port.match(/([0-9])+/g)[0],
+        protocol: "SOCKS" + protocol.match(/([0-9]+)/g).reverse()[0],
+        responseTime: +responseTimes.match(/([0-9])+/g)[0]
+      });
     });
 
-    const [, ipAddress, port, , , protocol, responseTimes] = rowData;
-    /**
-     * IP address and port are both stored in a strange script tag
-     * I think to make it harder to scrape, I guess if they change
-     * it will break, but seems fine for now, pretty simple regex.
-     *
-     * Possible socks Num formats are currently: "socks4", "socks4/5", "socks5".
-     */
-    proxyList.push({
-      ipAddress: ipAddress.match(/([0-9]+\.?)+/g)[0],
-      port: port.match(/([0-9])+/g)[0],
-      protocol: "SOCKS" + protocol.match(/([0-9]+)/g).reverse()[0],
-      responseTimes: responseTimes.match(/([0-9])+/g)[0]
-    });
-  });
+    return proxyList;
+  }
 
-  return proxyList;
+  async getProxy() {
+    if (!this.proxy) {
+      const proxyList = await this.getProxyList();
+      // Order by shortest response time
+      this.proxy = proxyList.sort((a, b) => a.responseTime > b.responseTime)[0];
+    }
+
+    return `${this.proxy.protocol}://${this.proxy.ipAddress}:${
+      this.proxy.port
+    }`;
+  }
 }
 
-module.exports = {
-  getProxyList
-};
+module.exports = new Proxy();
